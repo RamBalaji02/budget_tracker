dfa1ba59743271ae098b939f0bc4877f208ad380
RamBalaji02 <rbalajis222@gmail.com>
2026-06-01 16:55:20 +0530

    updated backend and frontend changes

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
 st.title("Expense Tracker")
-st.write("App is running 🚀")
+
+page = st.sidebar.selectbox("Menu", ["Dashboard", "Add Expense", "History"])
+
+if page == "Dashboard":
+    st.write("Dashboard Page")
+
+elif page == "Add Expense":
+    st.write("Add Expense Page")
+
+elif page == "History":
+    st.write("History Page")
