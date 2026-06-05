c946646b39de6dabeb1404eaac74ce921bc797b2
RamBalaji02 <rbalajis222@gmail.com>
2026-06-05 20:19:47 +0530

    add new lines

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/expense_tracker"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
