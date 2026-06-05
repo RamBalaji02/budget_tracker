7199f20d60f63c841036e8709c77bec6ec42347f
RamBalaji02 <rbalajis222@gmail.com>
2026-06-05 20:47:51 +0530

    add new lines

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.income import router as income_router
+from app.api.summary import router as summary_router
+
 
 app = FastAPI(title="Expense Tracker API")
 
 app.include_router(auth_router)
 app.include_router(income_router)
+app.include_router(summary_router)
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
