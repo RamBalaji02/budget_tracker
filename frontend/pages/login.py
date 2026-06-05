1838ac86b6d5f0e79367b2c245411172f801d28b
RamBalaji02 <rbalajis222@gmail.com>
2026-06-05 22:24:30 +0530

    add new lines

--- frontend/pages/login.py
+++ frontend/pages/login.py
@@ -5,28 +5,50 @@ st.title("🔐 Expense Tracker")
 
 tab1, tab2 = st.tabs(["Login", "Register"])
 
+# ---------------- LOGIN ----------------
 with tab1:
-    user = st.text_input("Username")
-    pwd = st.text_input("Password", type="password")
+    st.subheader("Login")
 
-    if st.button("Login"):
-        res = post("/login", {"username": user, "password": pwd})
+    login_user = st.text_input("Username", key="login_user")
+    login_pass = st.text_input("Password", type="password", key="login_pass")
+
+    if st.button("Login", key="login_btn"):
+        res = post("/login", {
+            "username": login_user,
+            "password": login_pass
+        })
 
         if res and res.status_code == 200:
             st.session_state["token"] = res.json()["access_token"]
-            st.success("Login successful")
+            st.success("Login successful 🎉")
             st.rerun()
         else:
-            st.error("Login failed")
+            try:
+                st.error(res.json().get("detail"))
+            except:
+                st.error("Login failed")
 
+# ---------------- REGISTER ----------------
 with tab2:
-    user = st.text_input("Username", key="r1")
-    pwd = st.text_input("Password", type="password", key="r2")
+    st.subheader("Register")
 
-    if st.button("Register"):
-        res = post("/register", {"username": user, "password": pwd})
+    reg_user = st.text_input("Username", key="reg_user")
+    reg_pass = st.text_input("Password", type="password", key="reg_pass")
+    reg_confirm = st.text_input("Confirm Password", type="password", key="reg_confirm")
 
-        if res and res.status_code == 200:
-            st.success("Registered successfully")
+    if st.button("Register", key="register_btn"):
+        if reg_pass != reg_confirm:
+            st.error("Passwords do not match")
         else:
-            st.error("Registration failed")
+            res = post("/register", {
+                "username": reg_user,
+                "password": reg_pass
+            })
+
+            if res and res.status_code in [200, 201]:
+                st.success("Account created successfully 🎉")
+            else:
+                try:
+                    st.error(res.json().get("detail"))
+                except:
+                    st.error("Registration failed")
