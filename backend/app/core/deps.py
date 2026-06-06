ef5e574f0ab4af67fbf3cb308cb79520f546572f
RamBalaji02 <rbalajis222@gmail.com>
2026-06-06 08:59:39 +0530

    add new lines

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -1,4 +1,12 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+
 from app.db.database import SessionLocal
+from app.core.security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +14,28 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        return user_id
+
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,13 +4,23 @@ from datetime import datetime, timedelta
 SECRET_KEY = "secret123"
 ALGORITHM = "HS256"
 
+
 def get_password_hash(password: str):
-    return password  # simple for now
+    return password
+
 
 def verify_password(plain, hashed):
     return plain == hashed
 
+
 def create_token(data: dict):
     payload = data.copy()
-    payload.update({"exp": datetime.utcnow() + timedelta(hours=2)})
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    payload.update({
+        "exp": datetime.utcnow() + timedelta(hours=2)
+    })
+
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
